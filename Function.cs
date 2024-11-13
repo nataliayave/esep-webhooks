9e8c46595a4517d605d0d69e6152c2a45f7e3368
Natalia Arroyave <116681140+nataliayave@users.noreply.github.com>
2024-11-13 00:14:58 -0500

    Update Function.cs

--- Function.cs
+++ Function.cs
@@ -11,13 +11,6 @@ namespace EsepWebhook;
 
 public class Function
 {
-    
-    /// <summary>
-    /// A simple function that takes a string and does a ToUpper
-    /// </summary>
-    /// <param name="input"></param>
-    /// <param name="context"></param>
-    /// <returns></returns>
     public string FunctionHandler(object input, ILambdaContext context)
     {
         context.Logger.LogInformation($"FunctionHandler received: 
